0b2bcc221982bf205fd8eeb0cb669c555f1437d7
Yiwen-Chan <yiwen_chan@foxmail.com>
2020-09-20 14:58:50 +0800

    Create config.go

---
+++ global/config.go
@@ -0,0 +1,67 @@
+package global
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type JsonConfig struct {
+	Master int64  `json:"master"`
+	Host   string `json:"host"`
+	Port   uint16 `json:"port"`
+}
+
+func DefaultConfig() *JsonConfig {
+	return &JsonConfig{
+		Master: 88888888,
+		Host:   "127.0.0.1",
+		Port:   2333,
+	}
+}
+
+func Load(p string) *JsonConfig {
+	if !PathExists(p) {
+		log.Warnf("尝试加载配置文件 %v 失败: 文件不存在", p)
+		return nil
+	}
+	c := JsonConfig{}
+	err := json.Unmarshal([]byte(ReadAllText(p)), &c)
+	if err != nil {
+		log.Warnf("尝试加载配置文件 %v 时出现错误: %v", p, err)
+		log.Infoln("原文件已备份")
+		os.Rename(p, p+".backup"+strconv.FormatInt(time.Now().Unix(), 10))
+		return nil
+	}
+	return &c
+}
+
+func (c *JsonConfig) Save(p string) error {
+	data, err := json.MarshalIndent(c, "", "\t")
+	if err != nil {
+		return err
+	}
+	WriteAllText(p, string(data))
+	return nil
+}
+
+func PathExists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil || os.IsExist(err)
+}
+
+func ReadAllText(path string) string {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return ""
+	}
+	return string(b)
+}
+
+func WriteAllText(path, text string) {
+	_ = ioutil.WriteFile(path, []byte(text), 0644)
+}
