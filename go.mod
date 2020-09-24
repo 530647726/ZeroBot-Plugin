45cf4cb6b52f65c81ce558027b408824ad0d9e8e
Yiwen-Chan <yiwen_chan@foxmail.com>
2020-09-24 14:33:10 +0800

    Add group welcome or sad

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module groupmanager
 go 1.14
 
 require (
+	github.com/Yiwen-Chan/qq-bot-api v0.0.0-20200922115134-4ae5f12d5e84 // indirect
 	github.com/mitchellh/mapstructure v1.3.3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sirupsen/logrus v1.6.0
--- main.go
+++ main.go
@@ -48,36 +48,46 @@ func main() {
 
 	log.Printf("GroupManager 启动完毕，正在运行")
 	for update := range updates {
+		if update.PostType == "notice" {
+			log.Printf("[群管系统] 收到：%s,%s", update.NoticeType, update.GroupID)
+			if update.NoticeType == "group_increase" {
+				message := "欢迎新人入群~"
+				bot.SendMessage(update.GroupID, "group", message)
+			}
+			if update.NoticeType == "group_decrease" {
+				message := "非常遗憾，有人退群了~"
+				bot.SendMessage(update.GroupID, "group", message)
+			}
+		}
 		if update.Message == nil {
 			continue
-		}
+		} else {
+			log.Printf("[%s] %s", update.Message.From.String(), update.Message.Text)
 
-		log.Printf("[%s] %s", update.Message.From.String(), update.Message.Text)
-
-		r := regexp.MustCompile("申请头衔(.+)")
-		if r.MatchString(update.Message.Text) {
-			parm := r.FindAllStringSubmatch(update.Message.Text, -1)
-			content := parm[0][1]
-			m, err := bot.SetChatMemberTitle(update.GroupID, update.UserID, content, 6000000000)
-			if err != nil {
-				log.Fatal(err)
+			r := regexp.MustCompile("申请头衔(.+)")
+			if r.MatchString(update.Message.Text) {
+				parm := r.FindAllStringSubmatch(update.Message.Text, -1)
+				content := parm[0][1]
+				m, err := bot.SetChatMemberTitle(update.GroupID, update.UserID, content, 6000000000)
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Printf("%s %s", "[群管系统] 申请头衔 ", m.Status)
+				if m.Status == "ok" {
+					message := "已修改"
+					bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+				} else {
+					message := "姬气人没有权限呢~"
+					bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+				}
 			}
-			log.Printf("%s %s", "[群管系统] 申请头衔 ", m.Status)
-			if m.Status == "ok" {
-				message := "已修改"
-				bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
-			} else {
-				message := "姬气人没有权限呢~"
-				bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+
+			if update.UserID != conf.Master {
+				continue
 			}
-		}
 
-		if update.UserID != conf.Master {
-			continue
-		}
-
-		if update.Message.Text == "群管系统" {
-			message := `=====>群管系统<=====
+			if update.Message.Text == "群管系统" {
+				message := `=====>群管系统<=====
 开启全员禁言
 解除全员禁言
 禁言[QQ]/[@QQ] [分钟]
@@ -90,193 +100,194 @@ func main() {
 私聊转发[QQ]/[@QQ] [内容]
 注：[QQ]和[内容]之间带空格
 ===================`
-			bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
-		}
-		if update.Message.Text == "开启全员禁言" {
-			m, err := bot.RestrictAllChatMembers(update.GroupID, true)
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Printf("%s %s", "[群管系统] 开启全员禁言 ", m.Status)
-			if m.Status == "ok" {
-				message := "已开启全群禁言"
-				bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
-			} else {
-				message := "姬气人没有权限呢~"
 				bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
 			}
-		}
-		if update.Message.Text == "解除全员禁言" {
-			m, err := bot.RestrictAllChatMembers(update.GroupID, false)
-			if err != nil {
-				log.Fatal(err)
+			if update.Message.Text == "开启全员禁言" {
+				m, err := bot.RestrictAllChatMembers(update.GroupID, true)
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Printf("%s %s", "[群管系统] 开启全员禁言 ", m.Status)
+				if m.Status == "ok" {
+					message := "已开启全群禁言"
+					bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+				} else {
+					message := "姬气人没有权限呢~"
+					bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+				}
 			}
-			log.Printf("%s %s", "[群管系统] 解除全员禁言 ", m.Status)
-			if m.Status == "ok" {
-				message := "已解除全群禁言"
-				bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
-			} else {
-				message := "姬气人没有权限呢~"
+			if update.Message.Text == "解除全员禁言" {
+				m, err := bot.RestrictAllChatMembers(update.GroupID, false)
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Printf("%s %s", "[群管系统] 解除全员禁言 ", m.Status)
+				if m.Status == "ok" {
+					message := "已解除全群禁言"
+					bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+				} else {
+					message := "姬气人没有权限呢~"
+					bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+				}
+			}
+			r = regexp.MustCompile("禁言(.+)\\s(.+)")
+			if r.MatchString(update.Message.Text) {
+				parm := r.FindAllStringSubmatch(update.Message.Text, -1)
+				target := findInt(parm[0][1])
+				time := time.Duration(findInt(parm[0][2])) * time.Minute
+				m, err := bot.RestrictChatMember(update.GroupID, target, time)
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Printf("%s %s", "[群管系统] 禁言 ", m.Status)
+				if m.Status == "ok" {
+					message := "小黑屋收留成功"
+					bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+				} else {
+					message := "姬气人没有权限呢~"
+					bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+				}
+			}
+			r = regexp.MustCompile("解除禁言(.+)")
+			if r.MatchString(update.Message.Text) {
+				parm := r.FindAllStringSubmatch(update.Message.Text, -1)
+				target := findInt(parm[0][1])
+				time := time.Duration(0) * time.Minute
+				m, err := bot.RestrictChatMember(update.GroupID, target, time)
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Printf("%s %s", "[群管系统] 解除禁言 ", m.Status)
+				if m.Status == "ok" {
+					message := "小黑屋释放成功"
+					bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+				} else {
+					message := "姬气人没有权限呢~"
+					bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+				}
+			}
+			r = regexp.MustCompile("踢出(.+)")
+			if r.MatchString(update.Message.Text) {
+				parm := r.FindAllStringSubmatch(update.Message.Text, -1)
+				target := findInt(parm[0][1])
+				m, err := bot.KickChatMember(update.GroupID, target, false)
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Printf("%s %s", "[群管系统] 踢出 ", m.Status)
+				if m.Status == "ok" {
+					message := "已踢出群聊，希望大家引以为戒"
+					bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+				} else {
+					message := "姬气人没有权限呢~"
+					bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+				}
+			}
+			r = regexp.MustCompile("退出群聊(.+)")
+			if r.MatchString(update.Message.Text) {
+				parm := r.FindAllStringSubmatch(update.Message.Text, -1)
+				target := findInt(parm[0][1])
+				m, err := bot.LeaveChat(target, update.Message.Chat.Type, false)
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Printf("%s %s", "[群管系统] 退出群聊 ", m.Status)
+				if m.Status == "ok" {
+					message := "已退出指定群聊"
+					bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+				} else {
+					message := "姬气人大失败~"
+					bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+				}
+			}
+			r = regexp.MustCompile("升为管理(.+)")
+			if r.MatchString(update.Message.Text) {
+				parm := r.FindAllStringSubmatch(update.Message.Text, -1)
+				target := findInt(parm[0][1])
+				m, err := bot.PromoteChatMember(update.GroupID, target, true)
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Printf("%s %s", "[群管系统] 升为管理 ", m.Status)
+				if m.Status == "ok" {
+					message := "已绿~"
+					bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+				} else {
+					message := "姬气人没有权限呢~"
+					bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+				}
+			}
+			r = regexp.MustCompile("取消管理(.+)")
+			if r.MatchString(update.Message.Text) {
+				parm := r.FindAllStringSubmatch(update.Message.Text, -1)
+				target := findInt(parm[0][1])
+				m, err := bot.PromoteChatMember(update.GroupID, target, false)
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Printf("%s %s", "[群管系统] 取消管理 ", m.Status)
+				if m.Status == "ok" {
+					message := "不绿了~"
+					bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+				} else {
+					message := "姬气人没有权限呢~"
+					bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+				}
+			}
+			r = regexp.MustCompile("修改群名片(.+)\\s(.+)")
+			if r.MatchString(update.Message.Text) {
+				parm := r.FindAllStringSubmatch(update.Message.Text, -1)
+				target := findInt(parm[0][1])
+				content := parm[0][2]
+				m, err := bot.SetChatMemberCard(update.GroupID, target, content)
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Printf("%s %s", "[群管系统] 修改群名片 ", m.Status)
+				if m.Status == "ok" {
+					message := "群名片已修改"
+					bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+				} else {
+					message := "姬气人没有权限呢~"
+					bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+				}
+			}
+			r = regexp.MustCompile("设置群头衔(.+)\\s(.+)")
+			if r.MatchString(update.Message.Text) {
+				parm := r.FindAllStringSubmatch(update.Message.Text, -1)
+				target := findInt(parm[0][1])
+				content := parm[0][2]
+				m, err := bot.SetChatMemberTitle(update.GroupID, target, content, 6000000000)
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Printf("%s %s", "[群管系统] 设置群头衔 ", m.Status)
+				if m.Status == "ok" {
+					message := "群头衔已设置"
+					bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+				} else {
+					message := "姬气人没有权限呢~"
+					bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
+				}
+			}
+			r = regexp.MustCompile("群聊转发(.+)\\s(.+)")
+			if r.MatchString(update.Message.Text) {
+				parm := r.FindAllStringSubmatch(update.Message.Text, -1)
+				target := findInt(parm[0][1])
+				content := parm[0][2]
+				bot.SendMessage(target, "group", content)
+				message := "发送成功喵~"
 				bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
 			}
-		}
-		r = regexp.MustCompile("禁言(.+)\\s(.+)")
-		if r.MatchString(update.Message.Text) {
-			parm := r.FindAllStringSubmatch(update.Message.Text, -1)
-			target := findInt(parm[0][1])
-			time := time.Duration(findInt(parm[0][2])) * time.Minute
-			m, err := bot.RestrictChatMember(update.GroupID, target, time)
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Printf("%s %s", "[群管系统] 禁言 ", m.Status)
-			if m.Status == "ok" {
-				message := "小黑屋收留成功"
-				bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
-			} else {
-				message := "姬气人没有权限呢~"
+			r = regexp.MustCompile("私聊转发(.+)\\s(.+)")
+			if r.MatchString(update.Message.Text) {
+				parm := r.FindAllStringSubmatch(update.Message.Text, -1)
+				target := findInt(parm[0][1])
+				content := parm[0][2]
+				bot.SendMessage(target, "private", content)
+				message := "发送成功喵~"
 				bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
 			}
-		}
-		r = regexp.MustCompile("解除禁言(.+)")
-		if r.MatchString(update.Message.Text) {
-			parm := r.FindAllStringSubmatch(update.Message.Text, -1)
-			target := findInt(parm[0][1])
-			time := time.Duration(0) * time.Minute
-			m, err := bot.RestrictChatMember(update.GroupID, target, time)
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Printf("%s %s", "[群管系统] 解除禁言 ", m.Status)
-			if m.Status == "ok" {
-				message := "小黑屋释放成功"
-				bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
-			} else {
-				message := "姬气人没有权限呢~"
-				bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
-			}
-		}
-		r = regexp.MustCompile("踢出(.+)")
-		if r.MatchString(update.Message.Text) {
-			parm := r.FindAllStringSubmatch(update.Message.Text, -1)
-			target := findInt(parm[0][1])
-			m, err := bot.KickChatMember(update.GroupID, target, false)
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Printf("%s %s", "[群管系统] 踢出 ", m.Status)
-			if m.Status == "ok" {
-				message := "已踢出群聊，希望大家引以为戒"
-				bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
-			} else {
-				message := "姬气人没有权限呢~"
-				bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
-			}
-		}
-		r = regexp.MustCompile("退出群聊(.+)")
-		if r.MatchString(update.Message.Text) {
-			parm := r.FindAllStringSubmatch(update.Message.Text, -1)
-			target := findInt(parm[0][1])
-			m, err := bot.LeaveChat(target, update.Message.Chat.Type, false)
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Printf("%s %s", "[群管系统] 退出群聊 ", m.Status)
-			if m.Status == "ok" {
-				message := "已退出指定群聊"
-				bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
-			} else {
-				message := "姬气人大失败~"
-				bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
-			}
-		}
-		r = regexp.MustCompile("升为管理(.+)")
-		if r.MatchString(update.Message.Text) {
-			parm := r.FindAllStringSubmatch(update.Message.Text, -1)
-			target := findInt(parm[0][1])
-			m, err := bot.PromoteChatMember(update.GroupID, target, true)
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Printf("%s %s", "[群管系统] 升为管理 ", m.Status)
-			if m.Status == "ok" {
-				message := "已绿~"
-				bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
-			} else {
-				message := "姬气人没有权限呢~"
-				bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
-			}
-		}
-		r = regexp.MustCompile("取消管理(.+)")
-		if r.MatchString(update.Message.Text) {
-			parm := r.FindAllStringSubmatch(update.Message.Text, -1)
-			target := findInt(parm[0][1])
-			m, err := bot.PromoteChatMember(update.GroupID, target, false)
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Printf("%s %s", "[群管系统] 取消管理 ", m.Status)
-			if m.Status == "ok" {
-				message := "不绿了~"
-				bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
-			} else {
-				message := "姬气人没有权限呢~"
-				bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
-			}
-		}
-		r = regexp.MustCompile("修改群名片(.+)\\s(.+)")
-		if r.MatchString(update.Message.Text) {
-			parm := r.FindAllStringSubmatch(update.Message.Text, -1)
-			target := findInt(parm[0][1])
-			content := parm[0][2]
-			m, err := bot.SetChatMemberCard(update.GroupID, target, content)
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Printf("%s %s", "[群管系统] 修改群名片 ", m.Status)
-			if m.Status == "ok" {
-				message := "群名片已修改"
-				bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
-			} else {
-				message := "姬气人没有权限呢~"
-				bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
-			}
-		}
-		r = regexp.MustCompile("设置群头衔(.+)\\s(.+)")
-		if r.MatchString(update.Message.Text) {
-			parm := r.FindAllStringSubmatch(update.Message.Text, -1)
-			target := findInt(parm[0][1])
-			content := parm[0][2]
-			m, err := bot.SetChatMemberTitle(update.GroupID, target, content, 6000000000)
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Printf("%s %s", "[群管系统] 设置群头衔 ", m.Status)
-			if m.Status == "ok" {
-				message := "群头衔已设置"
-				bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
-			} else {
-				message := "姬气人没有权限呢~"
-				bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
-			}
-		}
-		r = regexp.MustCompile("群聊转发(.+)\\s(.+)")
-		if r.MatchString(update.Message.Text) {
-			parm := r.FindAllStringSubmatch(update.Message.Text, -1)
-			target := findInt(parm[0][1])
-			content := parm[0][2]
-			bot.SendMessage(target, "group", content)
-			message := "发送成功喵~"
-			bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
-		}
-		r = regexp.MustCompile("私聊转发(.+)\\s(.+)")
-		if r.MatchString(update.Message.Text) {
-			parm := r.FindAllStringSubmatch(update.Message.Text, -1)
-			target := findInt(parm[0][1])
-			content := parm[0][2]
-			bot.SendMessage(target, "private", content)
-			message := "发送成功喵~"
-			bot.SendMessage(update.Message.Chat.ID, update.Message.Chat.Type, message)
 		}
 	}
 }
