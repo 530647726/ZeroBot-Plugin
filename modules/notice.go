f1be9626f34bad500080316486c1337827dddede
Yiwen-Chan <yiwen_chan@foxmail.com>
2020-09-25 20:42:52 +0800

    Add poke

---
+++ modules/notice.go
@@ -0,0 +1,43 @@
+package modules
+
+import (
+	"fmt"
+	"github.com/Yiwen-Chan/qq-bot-api"
+	"groupmanager/global"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func Group_increase_notice(bot *qqbotapi.BotAPI, conf *global.JsonConfig, update qqbotapi.Update) {
+	if update.PostType == "notice" {
+		log.Printf("[群管系统] 收到：%s,%s", update.NoticeType, update.GroupID)
+		if update.NoticeType == "group_increase" {
+			message := "欢迎新人入群~"
+			bot.SendMessage(update.GroupID, "group", message)
+		}
+	}
+}
+
+func Group_decrease_notice(bot *qqbotapi.BotAPI, conf *global.JsonConfig, update qqbotapi.Update) {
+	if update.PostType == "notice" {
+		log.Printf("[群管系统] 收到：%s,%s", update.NoticeType, update.GroupID)
+		if update.NoticeType == "group_decrease" {
+			message := "非常遗憾，有人退群了~"
+			bot.SendMessage(update.GroupID, "group", message)
+		}
+	}
+}
+
+func Notify_notice(bot *qqbotapi.BotAPI, conf *global.JsonConfig, update qqbotapi.Update) {
+	if update.PostType == "notice" {
+		log.Printf("[群管系统] 收到：%s,%s", update.NoticeType, update.GroupID)
+		if update.NoticeType == "notify" {
+			time.Sleep(time.Second * 2)
+			message := "请不要戳我 >_<"
+			bot.SendMessage(update.GroupID, "group", message)
+			message = fmt.Sprintf("[CQ:poke,qq=%v]", update.UserID)
+			bot.SendMessage(update.GroupID, "group", message)
+		}
+	}
+}
